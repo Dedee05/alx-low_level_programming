4db67d786a68629293740ca01785bd0b68e9df31
Dedee05 <dacychabs@gmail.com>
2023-05-24 09:52:14 -0700

    program that prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints base 10 numbers starting from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; i < 58; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
